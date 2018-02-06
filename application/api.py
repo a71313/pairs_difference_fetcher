94abaa7026736884c2ef594e5c5410467ba52a62
Alexey Kuznetsov <alexey.kuznetsov@bostongene.com>
2018-02-06 21:17:33 +0300

    small fix

--- application/api.py
+++ application/api.py
@@ -3,7 +3,10 @@ from aiohttp import web
 
 from application import Application
 
+
 @inject.params(app=Application)
 async def get_diff(request, app):
     data = await app.get_currency_difference()
+    if not data:
+        return web.json_response(data='Temporary unavailable')
     return web.json_response(data=data)
